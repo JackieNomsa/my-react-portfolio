cb47f26464528d8c053e58a9e0919af19cad4f16
jackie dube <jackienomsa3@gmail.com>
2023-09-28 18:34:46 +0200

    contact styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,16 @@ function Footer(){
     return(
         <div className="my_footer">
             <div className="my-sm-links">
-          <a href="https://github.com/JackieNomsa" target="_blank">
+          <a href="https://github.com/JackieNomsa" target="_blank" className="github">
             <IoLogoGithub />
           </a>
-          <a href="https://www.linkedin.com/in/jackie-nomsa-dube-390b66131" target="_blank">
+          <a href="https://www.linkedin.com/in/jackie-nomsa-dube-390b66131" target="_blank" className="linkedin">
             <IoLogoLinkedin />
           </a>
-          <a href="https://wa.me/0670769436" target="_blank">
+          <a href="https://wa.me/0670769436" target="_blank" className="whatsapp">
             <IoLogoWhatsapp />
           </a>
-          <a href="mailto:jackienomsadube@gmail.com" target="_blank">
+          <a href="mailto:jackienomsadube@gmail.com" target="_blank" className="email">
             <HiOutlineMail />
           </a>
         </div>
